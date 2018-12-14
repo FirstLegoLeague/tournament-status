a157bddc24b2721c2c8ed9563f21dcb96e97ea63
Tal Taub <taltaub22@gmail.com>
2018-12-14 11:02:30 +0200

    Refactoring the timer

--- src/js/components/Timer.jsx
+++ src/js/components/Timer.jsx
@@ -1,159 +1,82 @@
-import React, { Component } from 'react';
-import CircularProgressbar from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import React, { Component } from 'react'
+import CircularProgressbar from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 import '../../css/components/times.css'
-import Messenger from '../services/messenger';
+import Messenger from '../services/messenger'
+import Environment from '../services/env'
+import axios from 'axios'
+
+const THRESHOLD_IN_MINUTES = 2
 
 class Timer extends Component {
 
+  constructor (props) {
+    super(props)
 
-  constructor(props) {
-    super(props);
-    this.updateTime = this.updateTime.bind(this);
-    this.componentDidMount = this.componentDidMount.bind(this);
-    this.counter = 0;
-    this.prevDiff = 0;
-    this.numberOfMatchesSent = 2;
-    this.nextUpData = {};
-    setInterval(this.updateTime, 100);
+    this.state = {
+      nextUpMatches: [],
+      millisecondsTillNextMatch: 0,
+      timeThreshold: THRESHOLD_IN_MINUTES * 60 * 1000
+    }
 
+    this.upcomingMatchesPromise = Environment.load().then(env => `${env.moduleTournamentUrl}/match/upcoming`).then(url => this.upcomingUrl = url).then(() => axios.get(this.upcomingUrl))
+
+    setInterval(this.updateTime.bind(this), 1000)
   }
 
-  getNumOfMatches() {
-    return this.numberOfMatchesSent;
-  }
+  componentDidMount () {
 
-  /*
-  * MHub "data" is of the form
-  * [{
-  *    matchNumber: number,
-  *    matchStartTime: string,
-  *    teams: Array {
-  *       number:number,
-  *       name:string,
-  *       table:number
-  *    }
-  * }]
-  * nextMatchTime has to be of the format "hh:mm"
-  *
-  */
+    Promise.resolve(this.upcomingMatchesPromise).then(response => {
+      this.setState({nextUpMatches: response.data.sort(this.sortMatchesByTime)})
+    })
 
-  componentDidMount() {
-
-    Messenger.on('CurrentMatch:reload', (data, msg) => {
-      console.info(data)
-      this.nextUpData = data.data;
+    Messenger.on('UpcomingMatches:reload', (data, msg) => {
+      this.setState({nextUpMatches: data.data.sort(this.sortMatchesByTime)})
       this.updateTime()
-    });
-
-    Messenger.on('clock:time', (data, msg) => {
-      this.clockTime = data.data.time
     })
 
   }
 
-  /*
-   * Update the display
-  */
-  updateTime() {
-    this.numberOfMatchesSent = this.nextUpData.length;
-    if (this.nextUpData[0]) {
-      let nextMatch = this.nextUpData.sort((match1, match2) => match1.matchNumber - match2.matchNumber)[0]
-      const nextMatchTimeDate = new Date(nextMatch['matchStartTime']);
-      let timeString = '00:00';
-      if (nextMatch['matchStartTime']) {
-        const parts1 = nextMatchTimeDate.toTimeString().split(' ');
-        const timePart = parts1[0].split(':').slice(0, 2);
-        timeString = `${timePart[0]}:${timePart[1]}`;
-      }
-
-      const d = new Date();
-
-
-      let diff = 0;
-      const splitTime = timeString.split(":");
-      if (splitTime.length === 2) {
-
-        let nextHour = Number.parseInt(splitTime[0], 10);
-        const nextMinute = Number.parseInt(splitTime[1], 10);
-        const nextUpDate = new Date(d.getFullYear(), d.getMonth(), d.getDate(), nextHour, nextMinute);
-        diff = (nextUpDate - d) / 1000;
-      }
-      if (this.counter === 10) {
-        this.setState({ diffTime: diff, diffCircle: diff });
-        this.prevDiff = diff;
-        this.counter = 0;
-      } else {
-        this.setState({ diffTime: this.prevDiff, diffCircle: diff });
-      }
-      this.counter++;
-      return diff;
-    } else {
-      return 0;
-    }
-
+  updateTime () {
+    const currentTime = new Date()
+    let millisecondsTillNextMatch = new Date(this.state.nextUpMatches[0].startTime).getTime() - currentTime.getTime()
+    this.setState({millisecondsTillNextMatch})
   }
 
-  render() {
+  render () {
 
     if (this.state) {
-      let percentage = Math.abs(this.state.diffCircle / 300.0 * 100.0);
+      let percentage = Math.abs(this.state.millisecondsTillNextMatch / 300.0 * 100.0)
 
+      let text = this.getTimeText(this.state.millisecondsTillNextMatch)
 
-      //console.log(this.state.diffTime % 60);
-      const seconds = (Math.floor(Math.abs(this.state.diffTime % 60)));
-      let secondsString = `${seconds.toFixed(0)}`;
-      let minutes = (Math.floor(Math.abs(this.state.diffTime / 60)));
-      let minuteString = `${minutes.toFixed(0)}`;
-      if (Math.abs(this.state.diffTime) > 3600) {
-        minutes = (Math.floor(Math.abs(this.state.diffTime / 60) % 60));
-        minuteString = `${minutes.toFixed(0)}`;
-      }
-
-      if (Math.abs(seconds) < 10) {
-        secondsString = `0${seconds}`
-      }
-      if (Math.abs(minutes) < 10) {
-        minuteString = `0${minutes}`
-      }
-      let text = `+${minuteString}:${secondsString}`;
-      if (this.state.diffTime > 3600) {
-        text = `+${Math.floor(Math.abs(this.state.diffTime / 3600))}:${minuteString}:${secondsString}`;
+      let timerclass = 'greenTime'
+      if (this.state.millisecondsTillNextMatch <= this.state.timeThreshold && this.state.millisecondsTillNextMatch > -this.state.timeThreshold) {
+        timerclass = 'yellowTime'
+      } else if (this.state.millisecondsTillNextMatch <= -this.state.timeThreshold) {
+        timerclass = 'redTime'
+      } else if (this.state.millisecondsTillNextMatch > this.state.timeThreshold) {
+        timerclass = 'greenTime'
       }
 
-      if (this.state.diffTime < 0) {
-        text = `-${Math.floor(Math.abs(this.state.diffTime / 60))}:${secondsString}`;
-      }
-      if (this.state.diffTime < -3600) {
-        text = `-${Math.floor(Math.abs(this.state.diffTime / 3600))}:${minuteString}:${secondsString}`;
-      }
-      let timerclass = "greenTime";
-      if (Math.floor(Math.abs(this.state.diffTime)) <= 30 && this.state.diffTime > 0) {
-        timerclass = "yellowTime";
-      } else if (this.state.diffTime <= 0) {
-        timerclass = "redTime";
-      } else if (this.state.diffTime > 30) {
-        timerclass = "greenTime";
-      }
-      /*<div style={{ width: '25em' }} id="jello" className="cell">*/      // </div>
       if (percentage <= 100.0) {
         return (
           <div className="progress-container">
             <CircularProgressbar className={timerclass}
-              percentage={percentage}
-              text={`${text}`}
+                                 percentage={percentage}
+                                 text={`${text}`}
             />
           </div>
-        );
+        )
       } else {
         return (
           <div className="progress-container">
             <CircularProgressbar className={timerclass}
-              percentage={100}
-              text={`${text}`}
+                                 percentage={100}
+                                 text={`${text}`}
             />
           </div>
-        );
+        )
       }
     } else {
       return (
@@ -162,6 +85,35 @@ class Timer extends Component {
     }
 
   }
+
+  padNumber (number, size) {
+    let s = String(number)
+    while (s.length < (size || 2)) {s = '0' + s}
+    return s
+  }
+
+  getTimeText (value) {
+    if (value) {
+      let milliseconds = value
+      let delta = Math.abs(milliseconds) / 1000
+      let days = Math.floor(delta / 86400)
+      delta -= days * 86400
+      let hours = Math.floor(delta / 3600) % 24
+      delta -= hours * 3600
+      let minutes = Math.floor(delta / 60) % 60
+      delta -= minutes * 60
+      let seconds = Math.floor(delta % 60)
+
+      let daysString = days !== 0 ? `${this.padNumber(days, 2)}:` : ''
+      return `${milliseconds < 0 ? '-' : ''}  ${daysString}${this.padNumber(hours, 2)}:${this.padNumber(minutes, 2)}:${this.padNumber(seconds, 2)}`
+    }
+
+    return 'Unknown'
+  }
+
+  sortMatchesByTime (match1, match2) {
+    return new Date(match1.startTime).getTime() - new Date(match2.startTime).getTime()
+  }
 }
 
-export default Timer;
+export default Timer
