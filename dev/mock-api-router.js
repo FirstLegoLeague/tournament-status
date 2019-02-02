9de3d799ae34c8e86ccae71193fc36221be45e1e
Idan Stark <idanstark42@gmail.com>
2019-02-02 15:56:17 +0200

    Better base - no need to run tournament on dev now

--- dev/mock-api-router.js
+++ dev/mock-api-router.js
@@ -4,9 +4,15 @@ const path = require('path')
 const cors = require('cors')
 const router = express.Router()
 
+const STAGE = 'qualification'
 
 function mockResponses() {
-	return JSON.parse(fs.readFileSync(path.resolve(__dirname, 'responses.json'), 'utf8'))
+	const res = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'responses.json'), 'utf8'))
+	res.matches.forEach(match => {
+		match.startTime = new Date(match.startTime)
+		match.endTime = new Date(match.endTime)
+	})
+	return res
 }
 
 router.use(cors())
@@ -16,6 +22,30 @@ router.get('/environment.json', (req, res, next) => {
 	res.send(responses.env)
 })
 
-// router.use(express.static(path.resolve(__dirname, '../dist')))
+router.get('/settings/tournamentStage', (req, res, next) => {
+	res.send(STAGE)
+})
+
+router.get('/table/all', (req, res, next) => {
+	let responses = mockResponses()
+	res.send(responses.tables)
+})
+
+router.get('/match/current', (req, res, next) => {
+	let responses = mockResponses()
+	res.send(responses.matches[0])
+})
+
+router.get('/match/upcoming/:count', (req, res, next) => {
+	let responses = mockResponses()
+	res.send(responses.matches.slice(1, req.params.count + 1))
+})
+
+router.get('/match/upcoming', (req, res, next) => {
+	let responses = mockResponses()
+	res.send(responses.matches.slice(1, 2))
+})
+
+router.use(express.static(path.resolve(__dirname, '../dist')))
 
 exports.MockAPIRouter = router
