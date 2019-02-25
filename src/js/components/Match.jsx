5138d2ba27f97ebd9fa9f986c997b22ebc86d830
Idan Stark <idanstark42@gmail.com>
2019-02-25 17:12:02 +0200

    match text-fit

--- src/js/components/Match.jsx
+++ src/js/components/Match.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Time from 'react-time-format'
 import '../../css/components/match.css'
 import {upperCaseFirstIfLetter} from '../classes/StringUtil'
+import { Textfit } from 'react-textfit'
 
 export default class Match extends Component {
 
@@ -19,8 +20,12 @@ export default class Match extends Component {
       matchTeams.push(
         <div className="cell auto">
           <div className="grid-y">
-            <div className="cell small-7 medium-7 large-7 auto">{matchTeam.tableName}</div>
-            <div className="cell small-5 medium-5 large-5 auto">{matchTeam.teamNumber}</div>
+            <Textfit className="cell small-7 medium-7 large-7 auto text-center" mode="single" max="20" forceSingleModeWidth="false">
+              {matchTeam.tableName}
+            </Textfit>
+            <Textfit className="cell small-7 medium-7 large-7 auto text-center" mode="single" max="20" forceSingleModeWidth="false">
+              {matchTeam.teamNumber}
+            </Textfit>
           </div>
         </div>
       )
@@ -29,10 +34,14 @@ export default class Match extends Component {
     return (
       <div className="match grid-y">
         <div className="cell bold">
-          {upperCaseFirstIfLetter(this.props.match.stage)} #{this.props.match.matchId}
+          <Textfit className="cell small-7 medium-7 large-7 auto text-center" mode="single" max="20" forceSingleModeWidth="false">
+            {upperCaseFirstIfLetter(this.props.match.stage)} #{this.props.match.matchId}
+          </Textfit>
         </div>
         <div className="cell italic border bottom">
-          <Time value={this.props.match.startTime} format="HH:mm:ss"/>
+          <Textfit className="cell small-7 medium-7 large-7 auto text-center" mode="single" max="20" forceSingleModeWidth="false">
+            <Time value={this.props.match.startTime} format="HH:mm:ss"/>
+          </Textfit>
         </div>
         <div className="cell">
           <div className="grid-x grid-padding-x">
